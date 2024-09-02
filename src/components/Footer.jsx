a954ba3655d6cb3363744cd9f7b226c798572bcc
Maroof Qureshi <developermaroof@gmail.com>
2024-09-02 22:11:26 +0500

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <hr className='border-black'/>
         <footer className='flex justify-center items-center text-center py-4'>
             <div className='flex justify-center items-center gap-2'>
-                <p>Designed with </p>
-                <FaHeart />
-                <p>by <span className='underline font-semibold'>Devnito</span></p>
+                <p className='md:text-lg lg:text-xl'>Designed with </p>
+                <FaHeart className='md:w-4 md:h-4 lg:w-5 lg:h-5'/>
+                <p className='md:text-lg lg:text-xl'>by <span className='underline font-semibold'>Devnito</span></p>
             </div>
         </footer>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import GoogleIcon from '../assets/google.png';
 
+
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState({});
@@ -59,16 +60,31 @@ const Home = () => {
           <div className="flex flex-col justify-center items-center p-10 gap-6 lg:gap-8">
             <h1 className='font-bold text-xl md:text-2xl lg:text-3xl'>Sign In</h1>
             <button onClick={GoogleSignUp} type="button" className="flex justify-center items-center text-center border p-2 md:p-3 gap-2 lg:gap-3 rounded">
-              <img src={GoogleIcon} alt="" className='w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10' width={100} height={100}/>
+              <img src={GoogleIcon} alt="googleicon" className='w-6 h-6 md:w-8 md:h-8 lg:w-10 lg:h-10' width={100} height={100}/>
               <p className='md:text-lg lg:text-xl'>Sign in with Google</p>
             </button>
           </div>
         )}
         {isLoggedIn && (
-          <div>
-            <button onClick={Logout} type="button" className="bg-red-500 text-white py-2 px-4 rounded">
+          <div className='p-4 flex flex-col gap-4 justify-center items-center text-center'>
+            <h1>Make your email list now, with Sales Email List Software from Devnito LLC.</h1>
+            <div className='flex flex-col gap-10'>
+
+              <div className='flex flex-col justify-center items-center w-52 px-4 rounded-lg shadow-[0_3px_10px_rgba(0,0,0,0.4)]'>
+                <div className='bg-black w-11 h-2 rounded-bl-3xl rounded-br-3xl mb-6'></div>
+                <img src={GoogleIcon} alt="" className='w-6 h-6' width={100} height={100}/>
+                <h2 className='mb-10'>View invoices</h2>
+              </div>
+
+              <div className='flex flex-col justify-center items-center w-52 px-4 rounded-lg shadow-[0_3px_10px_rgba(0,0,0,0.4)]'>
+                <div className='bg-black w-11 h-2 rounded-bl-3xl rounded-br-3xl mb-6'></div>
+                <img src={GoogleIcon} alt="" className='w-6 h-6' width={100} height={100}/>
+                <h2 className='mb-10'>View invoices</h2>
+              </div>
+            </div>
+            {/* <button onClick={Logout} type="button" className="border px-2 bg-blue-200">
               Log out
-            </button>
+            </button> */}
           </div>
         )}
       </div>
