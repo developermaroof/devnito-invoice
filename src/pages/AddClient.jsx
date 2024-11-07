be31edd893c719d75ecdf43b0cd992ddacd16549
Maroof Qureshi <developermaroof@gmail.com>
2024-11-07 18:48:20 +0500

    Updated and Added  ClientsList and AddClient Logic

--- src/pages/AddClient.jsx
+++ src/pages/AddClient.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-// import React, { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { collection, addDoc } from 'firebase/firestore';
-import { auth, db } from '../firebase/firebaseConfig';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { auth, db, storage } from '../firebase/firebaseConfig';
+import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 const AddClient = () => {
   const initialFormState = {
@@ -11,20 +11,32 @@ const AddClient = () => {
     email: '',
     number: '',
     country: '',
+    userProfile: null,
     source: '',
     website: '',
     projectsCompleted: '',
     earnings: '',
+    currentDate: '',  // Store raw date input here
     linkedin: '',
   };
+  const [logoURL, setLogoURL] = useState(null); // New state for logo URL
 
   const [formData, setFormData] = useState(initialFormState);
   const [additionalFields, setAdditionalFields] = useState([]);
   const navigate = useNavigate();
   
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    const { name, value, files } = e.target;
+    if (name === 'userProfile' && files && files[0]) {
+      const file = files[0];
+      setFormData({ ...formData, [name]: file }); // Set the file object
+  
+      // Create a preview URL for the file
+      const fileURL = URL.createObjectURL(file);
+      setLogoURL(fileURL); // Set the logo URL for preview
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleAddField = () => {
@@ -40,32 +52,53 @@ const AddClient = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const user = auth.currentUser;
-    if (!user) {
-      alert('You must be logged in to add a client.');
-      return;
-    }
-
     try {
+      const user = auth.currentUser;
+      if (!user) {
+        alert('You must be logged in to add a client.');
+        return;
+      }
+  
+      // Format the date before submission
+      const formattedDate = formData.currentDate
+        ? new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }).format(new Date(formData.currentDate))
+        : '';
+  
+      let logoUrl = '';
+  
+      if (formData.userProfile) {
+        // Upload new logo if a file is selected
+        const storageRef = ref(storage, `profiles/${formData.userProfile.name}`);
+        await uploadBytes(storageRef, formData.userProfile);
+        logoUrl = await getDownloadURL(storageRef); // Get the URL for the uploaded logo
+      }
+  
       const clientData = {
         ...formData,
+        userProfile: logoUrl, // Store the uploaded logo URL
+        currentDate: formattedDate, 
         userId: user.uid,
-        additionalFields, // Include dynamic fields
+        additionalFields,
       };
-
+  
       await addDoc(collection(db, 'clients'), clientData);
       alert('Client added successfully!');
-      setFormData(initialFormState); // Reset form after submission
-      setAdditionalFields([]); // Clear additional fields
-      navigate('/clientslist'); // Navigate to your client list page
+      setFormData(initialFormState);
+      setLogoURL(null); // Reset the logo preview
+      setAdditionalFields([]);
+      navigate('/clientslist');
     } catch (error) {
       console.error('Error adding client: ', error);
       alert(`Error adding client: ${error.message}`);
     }
   };
 
+
   return (
-    <>
     <div className='mb-10 w-[100%] border-2 border-red-300'>
       <h1 className='font-semibold text-center'>Add Client</h1>
       <form onSubmit={handleSubmit} className='border-yellow-500 border-2'>
@@ -85,6 +118,25 @@ const AddClient = () => {
           <label className='block mb-1 lg:text-xl xl:text-2xl'>Country:</label>
           <input className='border border-gray-300 p-2 rounded w-full lg:text-lg xl:text-xl' type="text" name="country" value={formData.country} onChange={handleChange} />
         </div>
+        <div className="col-span-full">
+          <label htmlFor="userProfile" className="block text-sm font-medium text-gray-900">
+            User Profile
+          </label>
+          <div className="mt-2 flex items-center gap-x-3">
+            {!logoURL ? (
+              <UserCircleIcon aria-hidden="true" className="h-12 w-12 text-gray-300" />
+            ) : (
+              <img src={logoURL} alt="Selected Logo" className="h-12 w-12 object-contain rounded-full" />
+            )}
+            <input
+              type="file"
+              name="userProfile"
+              accept="image/*"
+              onChange={handleChange}
+              className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            />
+          </div>
+        </div>
         <div>
           <label className='block mb-1 lg:text-xl xl:text-2xl'>Source:</label>
           <input className='border border-gray-300 p-2 rounded w-full lg:text-lg xl:text-xl' type="text" name="source" value={formData.source} onChange={handleChange} />
@@ -100,6 +152,16 @@ const AddClient = () => {
         <div>
           <label className='block mb-1 lg:text-xl xl:text-2xl'>Earnings:</label>
           <input className='border border-gray-300 p-2 rounded w-full lg:text-lg xl:text-xl' type="text" name="earnings" value={formData.earnings} onChange={handleChange} />
+        </div>
+        <div>
+          <label className='block mb-1 lg:text-xl xl:text-2xl'>Current Date:</label>
+          <input
+            className='border border-gray-300 p-2 rounded w-full lg:text-lg xl:text-xl'
+            type='date'
+            name="currentDate"
+            value={formData.currentDate}
+            onChange={handleChange}
+          />
         </div>
         <div>
           <label className='block mb-1 lg:text-xl xl:text-2xl'>LinkedIn:</label>
@@ -131,7 +193,6 @@ const AddClient = () => {
         <button type="submit" className='bg-primary text-white text-xs lg:text-lg xl:text-xl px-8 lg:px-10 py-2 rounded-3xl'>Add Client</button>
       </form>
     </div>
-    </>
   );
 };
 
--- src/pages/ClientsList.jsx
+++ src/pages/ClientsList.jsx
@@ -4,65 +4,96 @@ import {
   CheckCircleIcon,
   ChevronRightIcon,
   EnvelopeIcon,
-} from '@heroicons/react/20/solid'
+} from '@heroicons/react/20/solid';
 
-
-const candidates = [
-  {
-    name: 'Emily Selman',
-    email: 'emily.selman@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    applied: 'January 7, 2020',
-    appliedDatetime: '2020-07-01T15:34:56',
-    status: 'Completed phone screening',
-  },
-  // More candidates...
-]
-
+import React, { useEffect, useState } from 'react';
+import { useClient } from '../context/ClientContext';
+import { useNavigate } from 'react-router-dom';
+import { auth, db } from '../firebase/firebaseConfig';
+import { query, collection, where, getDocs } from 'firebase/firestore';
 
 const ClientsList = () => {
+  const { clients, setClients } = useClient();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  console.log("clients data:", clients);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      const user = auth.currentUser;
+      if (!user) {
+        navigate('/login');
+        return;
+      }
+
+      setLoading(true);
+      try {
+        const q = query(collection(db, 'clients'), where('userId', '==', user.uid));
+        const querySnapshot = await getDocs(q);
+        const fetchedClients = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setClients(fetchedClients);
+      } catch (error) {
+        console.error('Error fetching clients:', error);
+        setError('Failed to fetch clients. Please try again later.');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchClients();
+  }, [setClients, navigate]);
 
   return (
-    <>
-      <div className="min-h-full">
-        <main className="pb-16 pt-8">
-          <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-
-            {/* Stacked list */}
+    <div className="min-h-full">
+      {error && <p className="text-red-500 text-center">{error}</p>}
+      <main className="pb-16 pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          {clients && clients.length === 0 && !loading ? (
+            <p className="text-left text-[12px] md:text-[14px] lg:text-[16px] xl:text-[18px]">
+              No clients available.
+            </p>
+          ) : (
             <ul className="mt-5 divide-y divide-gray-200 border-t border-gray-200 sm:mt-0 sm:border-t-0">
-              {candidates.map((candidate) => (
-                <li key={candidate.email}>
+              {clients?.map((client) => (
+                <li key={client.id}>
                   <a href="/" className="group block">
                     <div className="flex items-center px-4 py-5 sm:px-0 sm:py-6">
                       <div className="flex min-w-0 flex-1 items-center">
                         <div className="flex-shrink-0">
                           <img
                             alt=""
-                            src={candidate.imageUrl}
+                            src={client.userProfile}
                             className="h-12 w-12 rounded-full group-hover:opacity-75"
                           />
                         </div>
                         <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                           <div>
-                            <p className="truncate text-sm font-medium text-purple-600">{candidate.name}</p>
+                            <p className="truncate text-sm font-medium text-purple-600">
+                              {client.name}
+                            </p>
                             <p className="mt-2 flex items-center text-sm text-gray-500">
-                              <EnvelopeIcon aria-hidden="true" className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" />
-                              <span className="truncate">{candidate.email}</span>
+                              <EnvelopeIcon
+                                aria-hidden="true"
+                                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                              />
+                              <span className="truncate">{client.email}</span>
                             </p>
                           </div>
                           <div className="hidden md:block">
                             <div>
                               <p className="text-sm text-gray-900">
-                                Applied on <time dateTime={candidate.appliedDatetime}>{candidate.applied}</time>
+                                Applied on ( {client.currentDate} )
                               </p>
                               <p className="mt-2 flex items-center text-sm text-gray-500">
                                 <CheckCircleIcon
                                   aria-hidden="true"
                                   className="mr-1.5 h-5 w-5 flex-shrink-0 text-green-400"
                                 />
-                                {candidate.status}
+                                Completed phone screening
                               </p>
                             </div>
                           </div>
@@ -79,77 +110,53 @@ const ClientsList = () => {
                 </li>
               ))}
             </ul>
+          )}
+          
+          {/* Pagination */}
+          <nav
+            aria-label="Pagination"
+            className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0"
+          >
+            <div className="-mt-px flex w-0 flex-1">
+              <a
+                href="/"
+                className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
+              >
+                <ArrowLongLeftIcon aria-hidden="true" className="mr-3 h-5 w-5 text-gray-400" />
+                Previous
+              </a>
+            </div>
+            <div className="hidden md:-mt-px md:flex">
+              {/* Pagination buttons */}
+              <a
+                href="/"
+                className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
+              >
+                1
+              </a>
+              <a
+                href="/"
+                aria-current="page"
+                className="inline-flex items-center border-t-2 border-purple-500 px-4 pt-4 text-sm font-medium text-purple-600"
+              >
+                2
+              </a>
+              {/* Other pages */}
+            </div>
+            <div className="-mt-px flex w-0 flex-1 justify-end">
+              <a
+                href="/"
+                className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
+              >
+                Next
+                <ArrowLongRightIcon aria-hidden="true" className="ml-3 h-5 w-5 text-gray-400" />
+              </a>
+            </div>
+          </nav>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-            {/* Pagination */}
-            <nav
-              aria-label="Pagination"
-              className="flex items-center justify-between border-t border-gray-200 px-4 sm:px-0"
-            >
-              <div className="-mt-px flex w-0 flex-1">
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  <ArrowLongLeftIcon aria-hidden="true" className="mr-3 h-5 w-5 text-gray-400" />
-                  Previous
-                </a>
-              </div>
-              <div className="hidden md:-mt-px md:flex">
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  1
-                </a>
-                {/* Current: "border-purple-500 text-purple-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" */}
-                <a
-                  href="/"
-                  aria-current="page"
-                  className="inline-flex items-center border-t-2 border-purple-500 px-4 pt-4 text-sm font-medium text-purple-600"
-                >
-                  2
-                </a>
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  3
-                </a>
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  4
-                </a>
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  5
-                </a>
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  6
-                </a>
-              </div>
-              <div className="-mt-px flex w-0 flex-1 justify-end">
-                <a
-                  href="/"
-                  className="inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-200 hover:text-gray-700"
-                >
-                  Next
-                  <ArrowLongRightIcon aria-hidden="true" className="ml-3 h-5 w-5 text-gray-400" />
-                </a>
-              </div>
-            </nav>
-
-          </div>
-        </main>
-      </div>
-    </>
-  )
-}
-
-export default ClientsList
+export default ClientsList;
